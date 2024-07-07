7272d22c570368381b9f0c9b36507de20803444d
Kevin Santacruz <ksantacr@espol.edu.ec>
2024-07-07 14:38:17 -0500

    chore(init): initial projects creation

---
+++ BLA.UserFlow.API/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BLA.UserFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok("");
+        }
+    }
+}
---
+++ BLA.UserFlow.Tests/UnitTests/API/Controllers/UsersControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using BLA.UserFlow.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BLA.UserFlow.Tests.UnitTests.API.Controllers;
+
+public class UsersControllerTests
+{
+    private readonly UsersController _usersController;
+    public UsersControllerTests()
+    {
+        _usersController = new UsersController();
+    }
+
+    [Fact]
+    [Trait("API", "Returns OK with user list")]
+    public void Should_Return_OKObjectResult_GetUsers()
+    {
+        var result = _usersController.GetUsers();
+
+        result.Should().BeOfType<OkObjectResult>();
+    }
+}
